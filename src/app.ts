0dfc16d188e8497d801f5b99e05f712189027dd6
limploc <udindermana9@gmail.com>
2025-07-30 22:37:00 +0800

    refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,5 +17,4 @@ app.use(globalErrorHandler);
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
   console.log("API Documentation: http://localhost:3000/api-docs");
-  console.log("Error Demo: http://localhost:3000/api/v1/demo/errors?errorType=validation");
 });
